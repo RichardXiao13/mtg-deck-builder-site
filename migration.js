4521b9fd52de536e4376521ec6b9530c3cb39282
Richard Xiao <richard.xiao13@gmail.com>
2021-12-26 02:05:37 -0600

    Created sqlite tables for Users and Decks

---
+++ migration.js
@@ -0,0 +1,20 @@
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./database/decks.sqlite");
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS Users (" +
+    "id INTEGER PRIMARY KEY, " +
+    "username TEXT NOT NULL, " +
+    "password TEXT NOT NULL" +
+    ")"
+);
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS Decks (" +
+    "id INTEGER PRIMARY KEY, " +
+    "name TEXT NOT NULL, " +
+    "path TEXT NOT NULL, " +
+    "user_id INTEGER NOT NULL, " +
+    "FOREIGN KEY (user_id) REFERENCES Users(id)" +
+    ")"
+);
